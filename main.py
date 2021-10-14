9a954d05c40c102cd1ae57f4a25d1551191c9079
kobi <kobikahn@gmail.com>
2021-10-14 19:49:16 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+
+Tokenize by splitting with white space
+First token is the key in the data base
+Logical operator is the second token
+Value is the third token
+Connector
+
+In order to test:
+1. Test if token[0] is a vaild key name
+2. Test if token[1] is a valid logical operator
+3. Test if token[2] is a valid value for this operation
+
+Use flow charts
+'''
+
+
+
+
+
+
+
+
