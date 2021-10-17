501f9365197f4a07a20831c7d29cab3ed9844f7f
kobi <kobikahn@gmail.com>
2021-10-16 22:02:28 -0400

    Finished up to the third test right before and/or

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 '''
 
 Tokenize by splitting with white space
@@ -17,7 +18,136 @@ Use flow charts
 
 
 
+valid = True
+
+valid_queries = ['department', 'age', 'gender', 'startdate', 'salary']
+
+valid_dict = {'department': ['==', '='], 'age' : ['>', '<', '==', '='] , 'gender' : ['==', '='], 'startdate': ['>', '<', '==', '='] , 'salary' : ['>', '<', '==', '=']}
 
 
+department_vals = ['sales', 'software', 'management']
+
+gender_vals = ['m', 'f']
+
+
+
+#### METHODS
+
+def error(valid):
+
+    print(valid, [])
+
+
+
+def test1(token1,valid_queries):
+    global valid
+    for value in valid_queries:
+        if token1.lower() == value:
+            valid = True
+            break
+        else:
+            valid = False
+
+    if valid == False:
+        print('ERROR WITH YOUR KEY NAME')
+        error(valid)
+    return valid
+
+
+
+def test2(token1, token2, valid_dict):
+    global valid
+    value_c = 0
+
+    for key, value in valid_dict.items():
+        if key == token1.lower() and value_c == 0:
+            for val in value:
+                if val == token2:
+                    valid = True
+                    value_c = 1
+                    break
+
+                else:
+                    valid = False
+
+
+    if valid == False:
+        print('ERROR WITH YOUR LOGICAL OPERATOR')
+        error(valid)
+
+    return valid
+
+
+
+def test3(token1, token3, department_vals, gender_vals):
+    global valid
+
+    if token1.lower() == 'age' or token1.lower() == 'salary' or token1.lower() == 'startdate':
+        if token3.isdigit():
+            valid = True
+        else:
+            valid = False
+
+    elif token1.lower() == 'department':
+        for value in department_vals:
+            if value == token3.lower():
+                valid = True
+                break
+            else:
+                valid = False
+
+    else:
+        for value in gender_vals:
+            if value == token3.lower():
+                valid = True
+                break
+            else:
+                valid = False
+
+    if valid == False:
+        print('ERROR WITH YOUR VALUE FOR THE REFERENCED KEY')
+        error(valid)
+
+    return valid
+
+
+
+
+
+
+
+
+def main(valid_queries, valid_dict, department_vals):
+    global valid
+    global run
+    query = input('Find all records with: ')
+
+
+    user_query = query.split()
+    size = len(user_query)
+
+
+    if size == 3 or size == 7:
+        pass
+    else:
+        print('ERROR WITH THE LENGTH OF QUERY')
+        valid = False
+        error(valid)
+
+    token1 = user_query[0]
+    token2 = user_query[1]
+    token3 = user_query[2]
+
+    if valid and test1(token1, valid_queries):
+        pass
+
+    if valid and test2(token1, token2, valid_dict):
+        pass
+
+    if valid and test3(token1, token3, department_vals, gender_vals):
+        pass
+
+
+main(valid_queries, valid_dict, department_vals)
 
 
